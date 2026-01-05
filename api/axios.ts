e96ecc6a61a6a24c87e9c3db5549035818df0d66
DuongDong <dong.dq225808@sis.hust.edu.vn>
2026-01-05 11:48:19 +0700

    fix error message login

--- api/axios.ts
+++ api/axios.ts
@@ -26,14 +26,3 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      removeAccessToken();
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
